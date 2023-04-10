a039043964a702f79faafc02ccc39ef8fbbc07b9
David Capela <david.bc2.24@gmail.com>
2023-04-10 15:40:43 +0100

    add first structre

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import kademlia.Kademlia;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("");
+
+        Kademlia kademlia = new Kademlia(23);
+        System.out.println(kademlia.idade);
+    }
+}
---
+++ src/main/java/kademlia/Kademlia.java
@@ -0,0 +1,14 @@
+package kademlia;
+
+public class Kademlia {
+
+    public int idade;
+
+
+    /*
+
+     */
+    public Kademlia(int idade) {
+        this.idade = idade;
+    }
+}

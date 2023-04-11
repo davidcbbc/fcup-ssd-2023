bcd430e1138e4d4cd655ebbba900b5768ee7dd17
Hilarious-99 <fabio.hilario.correia@gmail.com>
2023-04-11 19:42:10 +0100

    default

---
+++ src/main/java/AuctionMechanism/User.java
@@ -0,0 +1,27 @@
+package AuctionMechanism;
+
+public class User {
+    private String address;
+    private double balance;
+
+    public User(String address, double balance) {
+        this.address = address;
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void addBalance(double amount) {
+        this.balance += amount;
+    }
+
+    public void subtractBalance(double amount) {
+        this.balance -= amount;
+    }
+}
